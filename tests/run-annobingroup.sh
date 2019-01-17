b548f4fe92c43e117cac395a8d23cb9c1fab02af
Ulf Hermann <ulf.hermann@qt.io>
2019-01-17 15:00:46 +0100

    tests: Use separate files for strip outputs
    This way we can verify that strip actually created all of them.
    Also, check that strip, unstrip, elflint actually succeed in
    run-strip-test-many.sh. This exposed the fact that the generated
    testfile in run-strip-test-many.sh is considered as illformed by
    strip -g.
    
    Signed-off-by: Ulf Hermann <ulf.hermann@qt.io>

--- tests/run-annobingroup.sh
+++ tests/run-annobingroup.sh
@@ -25,7 +25,7 @@
 # gcc -g -O2 -fplugin=annobin -c testfile-annobingroup.c
 testfiles testfile-annobingroup.o
 
-tempfiles merged.elf stripped.elf debugfile.elf remerged.elf
+tempfiles merged.elf stripped.elf debugfile1.elf debugfile2.elf debugfile3.elf remerged.elf
 
 testrun_compare ${abs_top_builddir}/src/readelf -g testfile-annobingroup.o << EOF
 
@@ -35,7 +35,7 @@ Section group [ 1] '.group' with signature '.text.unlikely.group' contains 3 ent
   [ 9] .text.unlikely
 EOF
 
-testrun ${abs_top_builddir}/src/strip -o stripped.elf -f debugfile.elf testfile-annobingroup.o
+testrun ${abs_top_builddir}/src/strip -o stripped.elf -f debugfile1.elf testfile-annobingroup.o
 
 testrun_compare ${abs_top_builddir}/src/readelf -g stripped.elf << EOF
 
@@ -45,7 +45,7 @@ Section group [ 1] '.group' with signature '.text.unlikely.group' contains 3 ent
   [ 9] .text.unlikely
 EOF
 
-testrun_compare ${abs_top_builddir}/src/readelf -g debugfile.elf << EOF
+testrun_compare ${abs_top_builddir}/src/readelf -g debugfile1.elf << EOF
 
 Section group [ 1] '.group' with signature '.text.unlikely.group' contains 3 entries:
   [ 7] .gnu.build.attributes..text.unlikely
@@ -53,7 +53,7 @@ Section group [ 1] '.group' with signature '.text.unlikely.group' contains 3 ent
   [ 9] .text.unlikely
 EOF
 
-testrun ${abs_top_builddir}/src/unstrip -o remerged.elf stripped.elf debugfile.elf
+testrun ${abs_top_builddir}/src/unstrip -o remerged.elf stripped.elf debugfile1.elf
 
 testrun_compare ${abs_top_builddir}/src/readelf -g remerged.elf << EOF
 
@@ -81,7 +81,7 @@ COMDAT section group [ 2] '.group' with signature '__x86.get_pc_thunk.ax' contai
   [13] .text.__x86.get_pc_thunk.ax
 EOF
 
-testrun ${abs_top_builddir}/src/strip -o stripped.elf -f debugfile.elf testfile-annobingroup-i386.o
+testrun ${abs_top_builddir}/src/strip -o stripped.elf -f debugfile2.elf testfile-annobingroup-i386.o
 
 testrun_compare ${abs_top_builddir}/src/readelf -g stripped.elf << EOF
 
@@ -94,7 +94,7 @@ COMDAT section group [ 2] '.group' with signature '__x86.get_pc_thunk.ax' contai
   [13] .text.__x86.get_pc_thunk.ax
 EOF
 
-testrun_compare ${abs_top_builddir}/src/readelf -g debugfile.elf << EOF
+testrun_compare ${abs_top_builddir}/src/readelf -g debugfile2.elf << EOF
 
 Section group [ 1] '.group' with signature '.text.unlikely.group' contains 3 entries:
   [ 8] .gnu.build.attributes..text.unlikely
@@ -105,7 +105,7 @@ COMDAT section group [ 2] '.group' with signature '__x86.get_pc_thunk.ax' contai
   [13] .text.__x86.get_pc_thunk.ax
 EOF
 
-testrun ${abs_top_builddir}/src/unstrip -o remerged.elf stripped.elf debugfile.elf
+testrun ${abs_top_builddir}/src/unstrip -o remerged.elf stripped.elf debugfile2.elf
 
 testrun_compare ${abs_top_builddir}/src/readelf -g remerged.elf << EOF
 
@@ -143,13 +143,13 @@ Section group [ 4] '.group' with signature '.text.unlikely..group' contains 1 en
   [27] .text.unlikely
 EOF
 
-testrun ${abs_top_builddir}/src/strip -o stripped.elf -f debugfile.elf testfile-annobingroup-x86_64.o
+testrun ${abs_top_builddir}/src/strip -o stripped.elf -f debugfile3.elf testfile-annobingroup-x86_64.o
 
 # This would/should work, except for the unknown NOTEs.
 # testrun ${abs_top_builddir}/src/elflint --gnu stripped.elf
-# testrun ${abs_top_builddir}/src/elflint --gnu --debug debugfile.elf
+# testrun ${abs_top_builddir}/src/elflint --gnu --debug debugfile3.elf
 
-testrun ${abs_top_builddir}/src/unstrip -o remerged.elf stripped.elf debugfile.elf
+testrun ${abs_top_builddir}/src/unstrip -o remerged.elf stripped.elf debugfile3.elf
 
 testrun ${abs_top_builddir}/src/elfcmp testfile-annobingroup-x86_64.o remerged.elf
 

426a000dc59bd824de86d20b1e4d772340031067
Frank Ch. Eigler <fche@redhat.com>
2020-03-26 21:32:40 -0400

    PR25583: debuginfod: conditionally prefer bsdtar to dpkg for .deb handling
    Prefer /usr/bin/dpkg-deb if installed, as normal on a debian system.
    
    Suggested-by: Mark Wielaard <mark@klomp.org>
    Signed-off-by: Frank Ch. Eigler <fche@redhat.com>

--- debuginfod/debuginfod.cxx
+++ debuginfod/debuginfod.cxx
@@ -429,8 +429,16 @@ parse_opt (int key, char *arg,
       scan_archives[".rpm"]="cat"; // libarchive groks rpm natively
       break;
     case 'U':
-      scan_archives[".deb"]="(bsdtar -O -x -f - data.tar.xz)<";
-      scan_archives[".ddeb"]="(bsdtar -O -x -f - data.tar.xz)<";
+      if (access("/usr/bin/dpkg-deb", X_OK) == 0)
+        {
+          scan_archives[".deb"]="dpkg-deb --fsys-tarfile";
+          scan_archives[".ddeb"]="dpkg-deb --fsys-tarfile";
+        }
+      else
+        {
+          scan_archives[".deb"]="(bsdtar -O -x -f - data.tar.xz)<";
+          scan_archives[".ddeb"]="(bsdtar -O -x -f - data.tar.xz)<";
+        }
       // .udeb too?
       break;
     case 'Z':

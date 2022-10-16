2a4ce08fafcf76d866ae5f6b394389d8d93aa0cb
Mark Wielaard <mark@klomp.org>
2022-10-16 18:23:06 +0200

    tests: include config.h first.
    Otherwise some symbols (lseek, open) might not get the 64bit offset
    variants because they don't pick up _FILE_OFFSET_BITS.
    
    Signed-off-by: Mark Wielaard <mark@klomp.org>

--- tests/dwfl-report-offline-memory.c
+++ tests/dwfl-report-offline-memory.c
@@ -15,8 +15,9 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-#include <assert.h>
 #include <config.h>
+
+#include <assert.h>
 #include <fcntl.h>
 #include <locale.h>
 #include <stdio.h>
